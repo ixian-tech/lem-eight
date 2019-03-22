a6b864ae7caf29482dc167391b6e155ce6e1bfeb
Alexander Guerra <alex@ixian.tech>
2019-03-22 07:12:21 -0500

    Setup redux tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import reduxThunk from "redux-thunk"
+
+function reducer(state, action) {
+    console.log("reducer", state, action)
+    return state
+}
+
+const store = createStore(
+    reducer,
+    composeWithDevTools()
+    // applyMiddleware(...reduxThunk)
+    // other store enhancers if any
+)
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
